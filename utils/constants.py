aa28600d31d8f3b698a627a98c0e024cb3b727e7
Jørn Tillnes <soundtrackgeekplus@gmail.com>
2024-11-02 10:50:46 +0100

    Add constants module for screen dimensions, colors, font settings, and game configurations

---
+++ utils/constants.py
@@ -0,0 +1,26 @@
+# utils/constants.py
+
+# Screen dimensions
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+
+# Colors
+COLOR_BLACK = (0, 0, 0)
+COLOR_WHITE = (255, 255, 255)
+COLOR_GRAY = (100, 100, 100)
+COLOR_RED = (255, 0, 0)
+COLOR_GREEN = (0, 255, 0)
+COLOR_BLUE = (0, 0, 255)
+
+# Font settings
+FONT_PATH = 'assets/fonts/c64_font.ttf'
+DEFAULT_FONT = 'Arial'
+
+# Frames per second
+FPS = 60
+
+# Menu settings
+MENU_OPTIONS_SPACING = 60
+
+# Save game path
+SAVE_GAME_PATH = 'savegames/game.db'
