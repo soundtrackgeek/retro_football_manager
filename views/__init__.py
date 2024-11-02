dc1b421f777c905261e60afa6a20d704def26c49
Jørn Tillnes <soundtrackgeekplus@gmail.com>
2024-11-02 10:55:33 +0100

    Add view imports and define __all__ for MenuView, TeamView, PlayerView, MatchView, LeagueView, FinanceView, and SettingsView

--- views/__init__.py
+++ views/__init__.py
@@ -1,1 +1,9 @@
-# views/__init__.py
+from .menu_view import MenuView
+from .team_view import TeamView
+from .player_view import PlayerView
+from .match_view import MatchView
+from .league_view import LeagueView
+from .finance_view import FinanceView
+from .settings_view import SettingsView
+
+__all__ = ['MenuView', 'TeamView', 'PlayerView', 'MatchView', 'LeagueView', 'FinanceView', 'SettingsView']
